7504f98ba04a16ca94c314c5ce782d038f302088
Rahul <rrahulkumaryadav347@gmail.com>
2023-03-10 19:02:38 +0530

    jhghjf

---
+++ server.js
@@ -0,0 +1,51 @@
+// get express 
+const express = require('express');
+//body parser for parsing fetch request body, else it will show undefined
+const bodyParser = require('body-parser');
+const {v4:uuidv4}=require("uuid")
+const path = require("path");
+const fileupload=require("express-fileupload")
+const app = express();
+
+let port = 5000||process.env.PORT;
+app.use(express.static(path.join(__dirname, './locationpage/build')));
+app.use(fileupload());
+
+app.use(express.static("uploads"))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const fileid=Date.now() + Math.random()
+
+
+app.post("/upload", (req, res) => {
+    const newpath = __dirname + "/uploads/";
+    const file = req.files.file;
+    const filename = fileid+file.name;
+   
+    file.mv(`${newpath}${filename}`, (err) => {
+      if (err) {
+        res.status(500).send({ message: "File upload failed", code: 200 });
+      }
+
+      res.status(200).send({ message: "File Uploaded", code: 200 , fileID:filename});
+    });
+  });
+
+app.get("/", (req, res) =>{
+    res.sendFile(path.join(__dirname, "./locationpage/build/index.html"));
+})
+
+app.get("/download",  (req, res)=> {
+  const imageID=req.query.id
+    // The res.download() talking file path to be downloaded
+    res.download(__dirname + "/uploads/"+imageID, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
+
+app.listen(port,()=>{
+    console.log("app is listening on" + port + " port")
+    })
